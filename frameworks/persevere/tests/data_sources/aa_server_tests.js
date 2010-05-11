e63e2af13b27438c06d82f49e6e29b49dd0fe953
Scott Cytacki <scott@concord.org>
2010-05-11 00:10:56 -0400

    Further refactoring to possibly support reusing helper methods.

--- frameworks/persevere/tests/data_sources/aa_server_tests.js
+++ frameworks/persevere/tests/data_sources/aa_server_tests.js
@@ -1,14 +1,6 @@
 // the file name is aa_ so that it loads first
 // I couldn't get sc_require to work here
 
-module("Persevere.ServerTests", {
-	setup: function() {
-	},
-	
-	teardown: function() {
-	}
-});
-
 var getUrl = function(url) {
 	return SC.Request.getUrl(url).set('isAsynchronous', NO).json()
 	    .header('Accept', 'application/json')
@@ -22,6 +14,26 @@ var postUrl = function(url, data) {
 		.send(data);	
 };
 
+module("Persevere.ServerTests", {
+	setup: function() {
+		var ServerTest = (window.ServerTest = SC.Object.create());
+		
+		ServerTest.createTestObjectClass = function () {
+			return postUrl('/testserver/Class/', {'id':'TestObject', 'extends': {'$ref':'../Class/Object'}});
+		};
+
+		ServerTest.deleteTestObjectClass = function () {
+			return SC.Request.deleteUrl("/testserver/Class/TestObject").json()
+				.set('isAsynchronous', NO)
+				.header('Accept', 'application/json')
+				.send();
+		};		
+	},
+	
+	teardown: function() {
+	}
+});
+
 test("Verify persevere is running", function() {
 		
   // make a request here to see what we get back
@@ -33,41 +45,26 @@ test("Verify persevere is running", function() {
   ok(results.length > 0, 'response is not empty');  
 });
 
-test("Verify we can create and delete a new Class", function() {
-  // make a request here to see what we get back
-  // http://localhost:4020/testserver/Class/
-	//   var response = SC.Request.getUrl("/testserver/Class/").set('isAsynchronous', NO).json()
-	//     .header('Accept', 'application/json')
-	// .send()
+test("Verify we can create and delete a TestObject Class", function() {
+
+    var response;
 
     // verify that there is nothing there to start with 
-    var response, results;
-
 	response = getUrl('/testserver/Class/TestObject');
     equals(response.status, 404, "TestObject Class doesn't exist");
 
-	// to add:
-	// POST http://localhost:8080/testserver/Class/
-	// content type: 	application/x-www-form-urlencoded
-	// {"id":"TestObject","extends":{"$ref":"../Class/Object"}}
-	// this is a json post, however it might not be in the same format that SC sends by default.
-	response = postUrl('/testserver/Class/', {'id':'TestObject', 'extends': {'$ref':'../Class/Object'}});
+	response = ServerTest.createTestObjectClass();
 	equals(response.status, 201, 'TestObject created');
-    console.log("results: " + response.get('body'));
 
     // Note the location returned is not correct it doesn't include the port number
     // I assume this is passed through from persevere
-    var locationUrl = response.header('Location');
-	console.log("creation location: " + locationUrl);
+    // var locationUrl = response.header('Location');
+	// console.log("creation location: " + locationUrl);
 
     // to delete:
     // DELETE http://localhost:8080/testserver/Class/TestObject
-    results = null;
-	response = SC.Request.deleteUrl("/testserver/Class/TestObject").json()
-		.set('isAsynchronous', NO)
-		.header('Accept', 'application/json')
-		.send();
-		
+	response = ServerTest.deleteTestObjectClass();
+			
 	// currently the webbrick doesn't handle the return value of the delete correctly
 	equals(response.status, 500, 'deleting TestObject response is ok, the proxying code doesn\'t handle this correctly');
 	
